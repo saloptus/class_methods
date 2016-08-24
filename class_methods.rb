e863b29c5e8d15e4ce75f3a4218d9149e9bf4bbb
Maya Wang <salome.neptune@gmail.com>
2016-08-24 12:36:16 -0700

    code demo in class

--- class_methods.rb
+++ class_methods.rb
@@ -26,13 +26,31 @@
 ###################################
 
 ## INSTANCE VARIABLES
+## ATTRIBUTES(adv.)
+
 
 ## INSTANCE METHODS
+  #behaviors
+#     # ex: make a post, send a message, like a poste, create a photo album
+# class User
+#   def initialize
+#     @name
+#   end
+#
+# #INITIALIZE METHOD
+#   def add_username(name)
+#     @name =name
+#   end
+# end
 
-      #INITIALIZE METHOD
+#.method
+#.class
+
+# user1 = User.new
+# puts user1.method
 
 
-## ATTRIBUTES
+
 
 
 # class Order
@@ -61,9 +79,18 @@
 ###################################
 
 #Class methods are called directly by the class and not by an instance of the class.
+#
+# class Kitten
+#   def self.say_meow # do not use initialize
+#     return "meow"
+#   end
+# end
+#
+# puts Kitten.say_meow.upcase # call directly on the class
+#reason see gem facker
 
-
-
+#do not need initialize method because we do not need to create individual instance of the method
+# like Kitten.new
 
 
 ###################################
@@ -71,41 +98,46 @@
 ###################################
 
 
-# class Pawn
-#   attr_reader :position
-#   def initialize(position)
-#     @position = position
-#   end
+class Pawn
+  attr_reader :position
+  def initialize(position)
+    @position = position
+  end
 #
 #   # This is the class method, it starts with self.
 #   # It is only called on the class directly Pawn.make_row
-#   def self.make_row(side)
-#     if side == "white"
-#       num = 2
-#     else
-#       num = 7
-#     end
-#
-#     pawns = []
-#     ("a".."h").each do |letter|
-#       pawns << self.new("#{letter}#{num}")
-#     end
-#
-#     pawns
-#   end
-# end
+  def self.make_row(side)
+    if side == "white"
+      num = 2
+    else
+      num = 7
+    end
+
+    pawns = []    # pick a row in a matrix
+    ("a".."h").each do |letter|
+      pawns << self.new("#{letter}#{num}") # create an object inside itself
+    end
+    pawns
+  end
+end
 #
 # #make one pawn
-# one_pawn = Pawn.new("A2")
-#
-# #make a whole row of pawns
+one_pawn = Pawn.new("A2")
+one_pawn.make_row("purple")
+# because self.make_row, you can only call it on the class Pawn. Can not call it on an individual object.
+# .new is a class method. pre-written self. method.
+
+# #
+# # #make a whole row of pawns
 # pawns = Pawn.make_row("black")
-#
-# #What is being stored in this local variable pawns?
-# print pawns
-#
+# #
+# # #What is being stored in this local variable pawns?
+# pawns.each do |pawn|
+#   puts pawn.position
+# end
+# #
 # #WHAT IS THIS DOING!?
-# puts pawns.shuffle.first.position
+puts pawns.shuffle.first.position
 
 
 
@@ -116,7 +148,7 @@
 
 
 #In Gems, like faker
-  # https://github.com/stympy/faker/blob/master/lib/faker/hacker.rb
+  #https://github.com/stympy/faker/blob/master/lib/faker/hacker.rb
 
 #When we get into databases, our data will be tied to a class. That class will have some premade class methods for us to use:  .find, .last, .where
 #Those class methods allow you to find specific objects of that class based on an specified attributes of that class.
